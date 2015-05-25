a980e18b31f0092559441ae6304c9ef73dc61e91
ChosenWell <trevor@chosenwell.ca>
2015-05-24 19:52:02 -0600

    MapDemo
    Some Tests and prescripting done to the Map Scene

---
+++ Assets/PlayAnimation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAnimation : MonoBehaviour
+{
+
+	public float timer = 0f;
+	
+	Animator anim;
+	// Use this for initialization
+	void Start ()
+	{
+		GetComponent<Animator> ().enabled = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (timer < Time.time) {
+			GetComponent<Animator> ().enabled = true;
+			Debug.Log ("3 Seconds Reached");
+		}
+	}
+}
